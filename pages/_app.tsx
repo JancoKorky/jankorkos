f5a683bc0da277e388191e6b80a432e3fc2fe3be
Ján Korkoš <jankorkos@gmail.com>
2023-01-21 23:31:35 +0100

    fix missing ssr provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "@/styles/index.scss";
 import type { AppProps } from "next/app";
+import { SSRProvider } from "react-bootstrap";
 import Navigation from "@/components/Navigation/Navigation";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SSRProvider>
       <Navigation />
       <Component {...pageProps} />
-    </>
+    </SSRProvider>
   );
 }
