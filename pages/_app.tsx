616e90187372ca3ecc380d2537a93869890ef5ff
Ján Korkoš <jankorkos@gmail.com>
2023-01-21 22:40:51 +0100

    add dependencies + configurations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/index.scss";
+import type { AppProps } from "next/app";
+import Navigation from "@/components/Navigation/Navigation";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navigation />
+      <Component {...pageProps} />
+    </>
+  );
 }
