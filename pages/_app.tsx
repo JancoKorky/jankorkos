1728ebb5e03a8ba92305fd30e6e93a2ca8d2640a
Ján Korkoš <jankorkos@gmail.com>
2023-01-22 20:38:40 +0100

    add styles, config navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-import "@/styles/index.scss";
-import type { AppProps } from "next/app";
-import { SSRProvider } from "react-bootstrap";
-import Navigation from "@/components/Navigation/Navigation";
+import '@/styles/index.scss';
+import type { AppProps } from 'next/app';
+import { SSRProvider } from 'react-bootstrap';
+import Container from '@/components/Container/Container';
+import Navigation from '@/components/Navigation/Navigation';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SSRProvider>
-      <Navigation />
-      <Component {...pageProps} />
+      <Container fluid={'xl'}>
+        <Navigation />
+        <Component {...pageProps} />
+      </Container>
     </SSRProvider>
   );
 }
