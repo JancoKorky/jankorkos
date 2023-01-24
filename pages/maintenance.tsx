1cd91fccc3acd71cc778054aa835eeecf711b3c7
Ján Korkoš <jankorkos@gmail.com>
2023-01-25 00:14:23 +0100

    add gif at maintenance page

--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image';
+
 const Maintenance = () => {
   return (
     <>
       <div className="carousel-dark">
-        <div className="container">
+        <div className="container text-center">
           <h1>Website under construction</h1>
+          <Image width="500" height="500" src="/poke.gif" alt="pokemon" />
         </div>
       </div>
     </>
