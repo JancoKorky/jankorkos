fe8570efee6893c42d76fe47b6e35d0bc85ce4b4
Jan Korkos <jankorkos@gmail.com>
2023-04-29 20:43:25 +0200

    Revert "fix maintenance-2"
    This reverts commit 0c6b673120d8817087d8d09fe9244131bbd071d5.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {{redirects(): [{permanent: boolean, destination: string, source: string}|null], reactStrictMode: boolean}} */
 const nextConfig = {
   reactStrictMode: true,
+
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === '1'
+        ? {
+            source: '/((?!maintenance).*)',
+            destination: '/maintenance',
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean);
+  },
 };
 
 module.exports = nextConfig;
